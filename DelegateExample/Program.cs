f52a103b132807c77c6e7b064887bbbe50776223
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-27 17:23:33 +0530

    [Ritvik]Add: Multi Cast Delegate example

--- DelegateExample/Program.cs
+++ DelegateExample/Program.cs
@@ -7,12 +7,18 @@
         {
 
             Log log = new Log();
+
+            LogDel LogTextToScreen, LogTextToFile;
+
+            LogTextToScreen = new LogDel(log.LogTextToScreen);
+
+            LogTextToFile = new LogDel(log.LogTextToFile);
+
+            LogDel multiLogDel = LogTextToScreen + LogTextToFile;//Multi cast Delegate
+
             Console.WriteLine("Please Enter Your Name ");
             string name = Console.ReadLine();
-
-
-            LogDel del = new LogDel(log.LogTextToScreen);//del refrence is pointing to LogTextToFile
-            del(name);
+            multiLogDel(name);
         }
-    }
+    } 
 }
