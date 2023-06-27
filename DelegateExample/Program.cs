98e5e0833a6e63925471f45991d60c55d4a74fc5
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-27 17:12:05 +0530

    [Ritvik]Add: Delegates uses as a refernce to the method

---
+++ DelegateExample/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace DelegateExample
+{
+    public class Program
+    {
+        delegate void LogDel(string text);//Delegate Initialization
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Name ");
+            string name = Console.ReadLine();
+
+
+            LogDel del = new LogDel(LogTextToFile);//del refrence is pointing to LogTextToFile
+            del(name);
+        }
+
+        static void LogTextToScreen(string text)
+        {
+            Console.WriteLine($"{DateTime.Now}: {text}");
+        }
+
+        static void LogTextToFile(string text)
+        {
+            using(StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"Log.txt"),true))
+            {
+                 sw.WriteLine($"{DateTime.Now}: {text}");
+            }
+        }
+    }
+}
