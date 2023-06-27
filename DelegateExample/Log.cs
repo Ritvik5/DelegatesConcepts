034e4705ce537c3985e4e31192c8c1b2224ebcb3
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-27 17:16:08 +0530

    [Ritvik]Add: Using non static methods for Delegate Reference

--- DelegateExample/Log.cs
+++ DelegateExample/Log.cs
@@ -8,6 +8,17 @@ namespace DelegateExample
 {
     public  class Log
     {
+        public void LogTextToScreen(string text)
+        {
+            Console.WriteLine($"{DateTime.Now}: {text}");
+        }
 
+        public void LogTextToFile(string text)
+        {
+            using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true))
+            {
+                sw.WriteLine($"{DateTime.Now}: {text}");
+            }
+        }
     }
 }
